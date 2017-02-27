fecf8fe462803c377c2a42e28c83ebe4b1d92f06
AliBakrOfficial <a.b01127630232@gmail.com>
2017-02-27 20:52:24 +0200

    Update m3loma-address-bar.js

--- m3loma-address-bar.js
+++ m3loma-address-bar.js
@@ -1,4 +1,2 @@
-if (document.domain = "www.m3loma.org") {
 $('head').append('<!-- Chrome, Firefox OS and Opera --> <meta name="theme-color" content="#62b712" /> <!-- Windows Phone --> <meta name="msapplication-navbutton-color" 
 content="#62b712" /> <!-- iOS Safari --> <meta name="apple-mobile-web-app-status-bar-style" content="#62b712" /> ');
-}
