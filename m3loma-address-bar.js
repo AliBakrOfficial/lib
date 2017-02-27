f23122caf3783e33ed2a27db76f6ac351ee1e4f8
AliBakrOfficial <a.b01127630232@gmail.com>
2017-02-27 19:35:38 +0200

    Create m3loma-address-bar.js

---
+++ m3loma-address-bar.js
@@ -0,0 +1,10 @@
+if (window.location.hostname = "www.w3schools.com") {
+$('head').append('
+<!-- Chrome, Firefox OS and Opera -->
+<meta name="theme-color" content="#62b712" />
+<!-- Windows Phone --> 
+<meta name="msapplication-navbutton-color" 
+content="#62b712" /> 
+<!-- iOS Safari -->
+<meta name="apple-mobile-web-app-status-bar-style" content="#62b712" /> ');
+}
