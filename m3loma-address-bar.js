90135a7e0e377ea145d7b885558d77abee576b47
AliBakrOfficial <a.b01127630232@gmail.com>
2017-02-27 20:39:54 +0200

    up

--- m3loma-address-bar.js
+++ m3loma-address-bar.js
@@ -1,10 +1,4 @@
-if (window.location.hostname = "www.m3loma.org") {
-$('head').append('
-<!-- Chrome, Firefox OS and Opera -->
-<meta name="theme-color" content="#62b712" />
-<!-- Windows Phone --> 
-<meta name="msapplication-navbutton-color" 
-content="#62b712" /> 
-<!-- iOS Safari -->
-<meta name="apple-mobile-web-app-status-bar-style" content="#62b712" /> ');
+if (window.location.hostname = "http://www.m3loma.org") {
+$('head').append('<!-- Chrome, Firefox OS and Opera --> <meta name="theme-color" content="#62b712" /> <!-- Windows Phone --> <meta name="msapplication-navbutton-color" 
+content="#62b712" /> <!-- iOS Safari --> <meta name="apple-mobile-web-app-status-bar-style" content="#62b712" /> ');
 }
